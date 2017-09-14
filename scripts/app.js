11f8c618827150caafab6d428a32574e2b245828
Albin Mörner <albin.morner@gmail.com>
2017-09-14 18:32:06 +0200

    Add menu and styling

---
+++ scripts/app.js
@@ -0,0 +1,24 @@
+let initialized = false;
+
+function onInitialClick(e) {
+	if (!initialized) {
+		initialized = true;
+		e.preventDefault();
+		document
+			.querySelector('body')
+			.classList
+			.add('initialized');
+	}
+}
+
+function onContractingClick() {
+	document
+		.querySelector('section')
+		.innerHTML = 'Contact: albin.morner@gmail.com';
+}
+
+function onInvestmentsClick() {
+	document
+		.querySelector('section')
+		.innerHTML = 'www.inswallet.se';
+}
