2e9416e005cc204a7f20f16ded29b870902308ea
Albin Mörner <albin.morner@gmail.com>
2017-09-21 14:28:09 +0200

    Add push state

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,62 @@
 let initialized = false;
+let currentPage;
 
 function onInitialClick(e) {
 	if (!initialized) {
+		history.pushState({ initialized: false }, null, null);
+		onInitialize();
+	}
+}
+
+function onInitialize() {
 		initialized = true;
-		e.preventDefault();
 		document
 			.querySelector('body')
 			.classList
 			.add('initialized');
-	}
 }
 
 function onContractingClick() {
+	if (currentPage !== 'contracting') {
+		currentPage = 'contracting';
+		history.pushState({ initialized: true, currentPage }, null, null);
+	}
+	showContracting();
+}
+
+function showContracting() {
 	document
 		.querySelector('section')
 		.innerHTML = 'Contact: albin.morner@gmail.com';
 }
 
-function onInvestmentsClick() {
+function onHoldingsClick() {
+	if (currentPage !== 'holdings') {
+		currentPage = 'holdings';
+		history.pushState({ initialized: true, currentPage }, null, null);
+	}
+	showHoldings();
+}
+
+function showHoldings() {
 	document
 		.querySelector('section')
 		.innerHTML = 'To be announced.';
 }
+
+window.addEventListener('popstate', ({ state }) => {
+	if (state.initialized === true) {
+		onInitialize();
+	} else {
+		initialized = false;
+		document
+			.querySelector('body')
+			.classList
+			.remove('initialized');
+	}
+	if (state.currentPage === 'holdings') {
+		showHoldings();
+	} else if (state.currentPage === 'contracting') {
+		showContracting();
+	}
+});
