e0729c5b1d2c4286b5cfadc729f8bb5dc05a20c4
Albin Morner <albin.morner@gmail.com>
2023-11-10 14:06:56 +0200

    Update for wise kyc idiocy

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,11 @@
 /* Routes */
-const CONTRACTING = 'contracting';
-const HOLDINGS = 'holdings';
-const HOME = 'home';
+const CONTRACTING = "contracting";
+const HOME = "home";
 
 /* Content */
 const TEXTS = {
-	[CONTRACTING]: 'Contact: albin.morner@gmail.com',
-	[HOLDINGS]: 'To be announced.',
-	[HOME]: 'Evil multinational conglomerate. Cuts down rainforests, pollutes lakes, etc.'
+  [CONTRACTING]: "Contact: albin.morner@gmail.com",
+  [HOME]: `I, Albin Mörner, am a software engineer who do contracting through my company Mörner Industries OÜ (https://ariregister.rik.ee/eng/company/16633322/M%C3%B6rner-Industries-O%C3%9C?search_id=731dc57&pos=1).`,
 };
 
 /* State */
@@ -16,64 +14,52 @@ let currentPage;
 
 /* Click handlers */
 function onInitialClick(e) {
-	if (!initialized) {
-		history.pushState({ initialized: false }, null, null);
+  if (!initialized) {
+    history.pushState({ initialized: false }, null, null);
 
-		updateStateAndRoute(HOME);
-		onInitialize();
-	}
+    updateStateAndRoute(HOME);
+    onInitialize();
+  }
 }
 
 function onHeaderClick() {
-		updateStateAndRoute(HOME);
+  updateStateAndRoute(HOME);
 }
 
 function onContractingClick() {
-		updateStateAndRoute(CONTRACTING);
-}
-
-function onHoldingsClick() {
-		updateStateAndRoute(HOLDINGS);
+  updateStateAndRoute(CONTRACTING);
 }
 
 /* Logic */
 function onInitialize() {
-		initialized = true;
-		document
-			.querySelector('body')
-			.classList
-			.add('initialized');
+  initialized = true;
+  document.querySelector("body").classList.add("initialized");
 }
 
 function updatePushState(route) {
-	if (currentPage !== route) {
-			currentPage = route;
-			history.pushState({ initialized: true, currentPage }, null, null);
-	}
+  if (currentPage !== route) {
+    currentPage = route;
+    history.pushState({ initialized: true, currentPage }, null, null);
+  }
 }
 
 function showRoute(route) {
-	document
-		.querySelector('section')
-		.innerHTML = TEXTS[route];
+  document.querySelector("section").innerHTML = TEXTS[route];
 }
 
 function updateStateAndRoute(route) {
-		updatePushState(route);
-		showRoute(route);
+  updatePushState(route);
+  showRoute(route);
 }
 
 /* Popstate */
-window.addEventListener('popstate', ({ state }) => {
-	if (state.initialized === true) {
-		onInitialize();
-	} else {
-		initialized = false;
-		document
-			.querySelector('body')
-			.classList
-			.remove('initialized');
-	}
-	
-	showRoute(state.currentPage);
+window.addEventListener("popstate", ({ state }) => {
+  if (state.initialized === true) {
+    onInitialize();
+  } else {
+    initialized = false;
+    document.querySelector("body").classList.remove("initialized");
+  }
+
+  showRoute(state.currentPage);
 });
